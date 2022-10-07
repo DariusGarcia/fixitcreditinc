4752f4c5748b8aa4b039f195bfdd228a4d485d87
Darius Garcia <dariusgarcia888@gmail.com>
2022-10-07 14:58:47 -0700

    fix: added priority to landing page image

--- components/landing.jsx
+++ components/landing.jsx
@@ -37,6 +37,7 @@ export default function LandingComp() {
 						className='w-full h-full object-cover'
 						src='https://images.unsplash.com/photo-1525130413817-d45c1d127c42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1920&q=60&&sat=-100'
 						alt=''
+						priority
 						layout='fill'
 					/>
 					<div
